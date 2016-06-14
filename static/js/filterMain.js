5b17f7efef24b3d9672e5a13dc79ff0b91eb490d
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2016-06-14 21:42:25 +0200

    start with gallery

--- static/js/filterMain.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('filterMain', function ($scope, $http) {
-    $http.get("http://localhost:8000/static/json/filterMain.json").then(function(response) {
-        $scope.entries = response.data.entries;
-    });
-});
