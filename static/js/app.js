eed4e985427ff355ae750bc760061237f942c1a6
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2016-11-13 13:29:59 +0100

    add first gallery view

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,25 @@
+var app = angular.module('myApp', []);
+app.controller('filterMain', function($scope, $http) {
+    $http.get("http://localhost:8000/static/json/filterMain.json").then(function(response) {
+        $scope.entries = response.data.entries;
+    });
+});
 
+app.controller('gallery', function($scope, $http) {
+    $http.get("http://localhost:8001/v0/articles").then(function(response) {
+        $scope.articles = response.data;
+
+        $scope.articles.map(
+            function(v, i) {
+                if (typeof v.pictures != 'undefined') {
+                    if ('cma0' in v.pictures) {
+                        v.picture = v.pictures['cma0'];
+                    } else if ('rma0' in v.pictures) {
+                        v.picture = v.pictures['rma0'];
+                    }
+                }
+                return v;
+            }
+        )
+    });
+});
