978e662b25629e31a85239fe8ef24b2f55e7d9fa
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2017-01-26 14:25:13 +0100

    replace ng-route by ui-router, add support for sub-pages

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,91 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ui.router']);
+
+app.config(function($stateProvider) {
+    var homeState = {
+        name: 'home',
+        url: '/home',
+        templateUrl: 'pages/home.html'
+    }
+
+    var materialState = {
+        name: 'material',
+        templateUrl: 'pages/material.html'
+    }
+
+    var materialDefaultState = {
+        name: 'material.default',
+        url: '/material',
+        templateUrl: 'pages/material.default.html'
+    }
+
+    var materialGarneState = {
+        name: 'material.garne',
+        url: '/material/garne',
+        templateUrl: 'pages/material.garne.html'
+    }
+
+    var materialPerlenState = {
+        name: 'material.perlen',
+        url: '/material/perlen,'
+        templateUrl: 'pages/material.perlen.html'
+    }
+
+    var kontaktState = {
+        name: 'kontakt',
+        url: '/kontakt',
+        templateUrl: 'pages/kontakt.html'
+    }
+
+    $stateProvider.state(homeState);
+    $stateProvider.state(materialState);
+    $stateProvider.state(materialDefaultState);
+    $stateProvider.state(materialGarneState);
+    $stateProvider.state(materialPerlenState);
+    $stateProvider.state(kontaktState);
+});
 
 
 // configure our routes
-app.config(function($routeProvider) {
-    $routeProvider
-        .when('/home', {
-            templateUrl: 'pages/home.html'
-        })
-        .when('/artikel', {
-            templateUrl: 'pages/artikel.html'
-        })
-        .when('/herstellung', {
-            templateUrl: 'pages/herstellung.html'
-        })
-        .when('/material', {
-            templateUrl: 'pages/material.html'
-        })
-        .when('/markt', {
-            templateUrl: 'pages/markt.html'
-        })
-        .when('/kontakt', {
-            templateUrl: 'pages/kontakt.html'
-        })
-        .when('/warenkorb', {
-            templateUrl: 'pages/warenkorb.html'
-        })
-        .when('/agb', {
-            templateUrl: 'pages/agb.html'
-        })
-        .when('/impressum', {
-            templateUrl: 'pages/impressum.html'
-        })
-                .otherwise({
-            templateUrl: 'pages/home.html'
-        });
-});
+/*
+  app.config(function($routeProvider) {
+  $routeProvider
+  .when('/home', {
+  templateUrl: 'pages/home.html'
+  })
+  .when('/artikel', {
+  templateUrl: 'pages/artikel.html'
+  })
+  .when('/herstellung', {
+  templateUrl: 'pages/herstellung.html'
+  })
+  .when('/material/garne', {
+  templateUrl: 'pages/garne.html'
+  })
+  .when('/material', {
+  templateUrl: 'pages/material.html'
+  })
+  .when('/markt', {
+  templateUrl: 'pages/markt.html'
+  })
+  .when('/kontakt', {
+  templateUrl: 'pages/kontakt.html'
+  })
+  .when('/warenkorb', {
+  templateUrl: 'pages/warenkorb.html'
+  })
+  .when('/agb', {
+  templateUrl: 'pages/agb.html'
+  })
+  .when('/impressum', {
+  templateUrl: 'pages/impressum.html'
+  })
+  .otherwise({
+  templateUrl: 'pages/home.html'
+  });
+  });
+*/
 
 app.controller('navMain', function($scope, $location) {
-    $scope.isActive = function(viewLocation) {
-        return viewLocation === $location.path();
-    };
 });
 
 app.controller('filterMain', function($scope, $http) {
