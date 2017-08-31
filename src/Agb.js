119c1290415e1c2d1022f8c86997fcb6d27eebb8
Fiona Koestler <fiona@koestler.ch>
2017-08-31 12:12:04 +0200

    agb: delete unnecessary div

--- src/Agb.js
+++ src/Agb.js
@@ -7,103 +7,100 @@ const Agb = () => (
         title="Allgemeine Geschäftsbedingungen"
         headImagePath={config.get('apiUrl') + 'Image/800p/content/head/artikel_13.jpg'}
     >
-        <div>
-            <div className="intro">
-                <div className="intro-desc wide">
-                    <h2>Allgemeines</h2>
-                    <p> Der Webshop geknüpft.ch bietet handverarbeitete Waren und Dienstleistungen an. Alle Verkäufe,
-                        Lieferungen und sonstige Leistungen erfolgen ausschliesslich nach Massgaben dieser Allgemeinen
-                        Geschäftsbedingung.
-                    </p>
-                    <p>Ihre Daten benötigen wir, um den Bestellprozess abzuwickeln. Ihre Daten werden
-                        nicht an Dritte weitergegeben.
-                    </p>
-                    <h3>Vertrag</h3>
-                    <p> Der Kaufvertrag kommt durch die Bestätigung der Bestellung per E-Mail seitens geknüpft.ch
-                        zustande. Sollte eine Ware nicht mehr verfügbar sein, lassen wir uns die Möglichkeit offen, vom
-                        Kaufvertrag zurückzutreten. In diesem Fall werden Sie von uns per E-Mail benachrichtigt.
-                    </p>
-                    <h3>Preise</h3>
-                    <p> Es gelten jeweils die im Webshop und auf der Bestellbestätigung aufgeführten Preise. Diese sind
-                        in Schweizer Franken und inkl. der gesetzlichen Mehrwertsteuer, soweit nicht ausdrücklich anders
-                        angegeben. Die Preise können im Webshop jederzeit angepasst werden. Preisirrtümer bleiben
-                        vorbehalten. Unsere Angebote sind freibleibend.
-                    </p>
-                    <h3>Zahlung per Vorauskasse</h3>
-                    <p> Sie bezahlen nach Erhalt der Bestellbestätigung und der Rechnung per E-Mail auf das von uns
-                        angegebene
-                        Konto per Onlineüberweisung.
-                    </p>
-                    <p> Möchten Sie mit einem Einzahlungsschein am Schalter einzahlen, senden wir Ihnen diesen zu gegen
-                        eine zusätzliche Gebühr von 3.- Franken (1.- Franken Porto, 2.- Franken Einzahlungsgebühr).
-                    </p>
-                    <h3>Lieferung</h3>
-                    <p> Bei Lagerbeständen liefern wir die Ware nach Zahlungseingang innerhalb von 2-5 Werktagen an die
-                        angegebene Lieferadresse.
-                    </p>
-                    <p> Bei Ware, welche noch gefertigt werden muss, liefern wir nach Zahlungseingang innerhalb von 2
-                        Wochen an die angegebene Lieferadresse. Bei umfangreichen Bestellungen behalten wir uns vor, die
-                        Lieferfrist zu verlängern. Wir teilen Ihnen dies bei der Bestellbestätigung mit.
-                    </p>
-                    <p>Fall es zu einer Lieferverzögerung kommen sollte, werden wir Sie umgehend informieren.
-                    </p>
-                    <h3>Versandkosten</h3>
-                    <p> Wir versenden mit der Schweizerischen Post mit A-Post-Brief oder PostPac Economy. Die
-                        Versandkosten beinhalten Porto und Verpackung.
-                    </p>
-                    <p> Falls die Ware per Brief innerhalb der Schweiz und ins Fürstentum Lichtenstein versendet werden
-                        kann, berechnen wir für den A-Post-Brief pauschal 2.- Franken. Es erfolgt kein Tracking. Falls
-                        Sie ein Tracking wünschen, versenden wir mit A-Post Plus. Dies kostet zusätzlich 2.50 Franken.
-                    </p>
-                    <p> Bei Versand mit PostPac Economy innerhalb der Schweiz und ins Fürstentum Lichtenstein verrechnen
-                        wir pauschal 8.- Franken. Es erfolgt ein Tracking.
-                    </p>
-                    <p> Bestellungen mit Versand innerhalb der Schweiz und ins Fürstentum Lichtenstein sind ab 80.-
-                        Franken versandkostenfrei.
-                    </p>
-                    <p> Der Versand ins Ausland ist auf Anfrage möglich.
-                    </p>
-                    <h3>Eigentumsvorbehalt</h3>
-                    <p>Bis zur vollständingen Zahlung des Kaufpreises bleiben alle Produkte im Eigentum von geknüpft.ch.
-                    </p>
-                    <h3>Mindestbestellmenge</h3>
-                    <p>Es gibt keine Mindestbestellmenge.
-                    </p>
-                    <h3>Form- und Farbabweichung</h3>
-                    <p>Bei den von geknüpft.ch verwendeten Materialien gibt es Form- und insbesondere Farbabweichungen.
-                        Diese sind naturgemäss, aufgrund unterschiedlicher Monitore und Druckereinstellungen zu erwarten
-                        und
-                        geben daher keinen Grund zur Beanstandung. Abbildungen in unserem Webshop sind unverbindlich.
-                        Wenn
-                        Sie sich betreffend Farbe absichern wollen, besuchen Sie uns am Markt.
-                    </p>
-                    <h3>Rücksendung</h3>
-                    <p>Rücksendungen müssen vorgängig mit geknüpft.ch abgesprochen werden. Rücksendungen sind nur
-                        möglich,
-                        falls geknüpft.ch wider bester Bemühungen fehlerhafte oder falsche Ware geliefert hat.
-                    </p>
-                    <h3>Haftung</h3>
-                    <p>Geknüpft.ch übernimmt keine Haftung für Schäden, die beim Transport der Ware entstehen.
-                        Reklamieren
-                        Sie diese umgehend bei der Post und bitte teilen Sie uns dies telefonisch oder schriftlich mit.
-                    </p>
-                    <h3>Änderung</h3>
-                    <p>Diese Allgemeine Geschäftsbedingung kann von geknüpft.ch jederzeit und ohne Angabe von Gründen
-                        geändert werden. Die Änderungen gelten nicht für bereits getätigte Bestellungen.
-                    </p>
-                    <h3>Gerichtsstand</h3>
-                    <p>Gerichtsstand ist 8304 Wallisellen. Es gilt Schweizer Recht.
-                    </p>
-                    <h3>Schlussbestimmung</h3>
-                    <p>Sollte eine Bestimmung dieser AGB unwirksam sein, bleiben die übrigen Bestimmungen
-                        unberührt. Für Streitigkeiten die über diese AGB hinausgehen, findet das Schweizerische
-                        Obligationenrecht Anwendung.
-                    </p>
-                </div>
+        <div className="intro">
+            <div className="intro-desc wide">
+                <h2>Allgemeines</h2>
+                <p> Der Webshop geknüpft.ch bietet handverarbeitete Waren und Dienstleistungen an. Alle Verkäufe,
+                    Lieferungen und sonstige Leistungen erfolgen ausschliesslich nach Massgaben dieser Allgemeinen
+                    Geschäftsbedingung.
+                </p>
+                <p>Ihre Daten benötigen wir, um den Bestellprozess abzuwickeln. Ihre Daten werden
+                    nicht an Dritte weitergegeben.
+                </p>
+                <h3>Vertrag</h3>
+                <p> Der Kaufvertrag kommt durch die Bestätigung der Bestellung per E-Mail seitens geknüpft.ch
+                    zustande. Sollte eine Ware nicht mehr verfügbar sein, lassen wir uns die Möglichkeit offen, vom
+                    Kaufvertrag zurückzutreten. In diesem Fall werden Sie von uns per E-Mail benachrichtigt.
+                </p>
+                <h3>Preise</h3>
+                <p> Es gelten jeweils die im Webshop und auf der Bestellbestätigung aufgeführten Preise. Diese sind
+                    in Schweizer Franken und inkl. der gesetzlichen Mehrwertsteuer, soweit nicht ausdrücklich anders
+                    angegeben. Die Preise können im Webshop jederzeit angepasst werden. Preisirrtümer bleiben
+                    vorbehalten. Unsere Angebote sind freibleibend.
+                </p>
+                <h3>Zahlung per Vorauskasse</h3>
+                <p> Sie bezahlen nach Erhalt der Bestellbestätigung und der Rechnung per E-Mail auf das von uns
+                    angegebene
+                    Konto per Onlineüberweisung.
+                </p>
+                <p> Möchten Sie mit einem Einzahlungsschein am Schalter einzahlen, senden wir Ihnen diesen zu gegen
+                    eine zusätzliche Gebühr von 3.- Franken (1.- Franken Porto, 2.- Franken Einzahlungsgebühr).
+                </p>
+                <h3>Lieferung</h3>
+                <p> Bei Lagerbeständen liefern wir die Ware nach Zahlungseingang innerhalb von 2-5 Werktagen an die
+                    angegebene Lieferadresse.
+                </p>
+                <p> Bei Ware, welche noch gefertigt werden muss, liefern wir nach Zahlungseingang innerhalb von 2
+                    Wochen an die angegebene Lieferadresse. Bei umfangreichen Bestellungen behalten wir uns vor, die
+                    Lieferfrist zu verlängern. Wir teilen Ihnen dies bei der Bestellbestätigung mit.
+                </p>
+                <p>Fall es zu einer Lieferverzögerung kommen sollte, werden wir Sie umgehend informieren.
+                </p>
+                <h3>Versandkosten</h3>
+                <p> Wir versenden mit der Schweizerischen Post mit A-Post-Brief oder PostPac Economy. Die
+                    Versandkosten beinhalten Porto und Verpackung.
+                </p>
+                <p> Falls die Ware per Brief innerhalb der Schweiz und ins Fürstentum Lichtenstein versendet werden
+                    kann, berechnen wir für den A-Post-Brief pauschal 2.- Franken. Es erfolgt kein Tracking. Falls
+                    Sie ein Tracking wünschen, versenden wir mit A-Post Plus. Dies kostet zusätzlich 2.50 Franken.
+                </p>
+                <p> Bei Versand mit PostPac Economy innerhalb der Schweiz und ins Fürstentum Lichtenstein verrechnen
+                    wir pauschal 8.- Franken. Es erfolgt ein Tracking.
+                </p>
+                <p> Bestellungen mit Versand innerhalb der Schweiz und ins Fürstentum Lichtenstein sind ab 80.-
+                    Franken versandkostenfrei.
+                </p>
+                <p> Der Versand ins Ausland ist auf Anfrage möglich.
+                </p>
+                <h3>Eigentumsvorbehalt</h3>
+                <p>Bis zur vollständingen Zahlung des Kaufpreises bleiben alle Produkte im Eigentum von geknüpft.ch.
+                </p>
+                <h3>Mindestbestellmenge</h3>
+                <p>Es gibt keine Mindestbestellmenge.
+                </p>
+                <h3>Form- und Farbabweichung</h3>
+                <p>Bei den von geknüpft.ch verwendeten Materialien gibt es Form- und insbesondere Farbabweichungen.
+                    Diese sind naturgemäss, aufgrund unterschiedlicher Monitore und Druckereinstellungen zu erwarten
+                    und
+                    geben daher keinen Grund zur Beanstandung. Abbildungen in unserem Webshop sind unverbindlich.
+                    Wenn
+                    Sie sich betreffend Farbe absichern wollen, besuchen Sie uns am Markt.
+                </p>
+                <h3>Rücksendung</h3>
+                <p>Rücksendungen müssen vorgängig mit geknüpft.ch abgesprochen werden. Rücksendungen sind nur
+                    möglich,
+                    falls geknüpft.ch wider bester Bemühungen fehlerhafte oder falsche Ware geliefert hat.
+                </p>
+                <h3>Haftung</h3>
+                <p>Geknüpft.ch übernimmt keine Haftung für Schäden, die beim Transport der Ware entstehen.
+                    Reklamieren
+                    Sie diese umgehend bei der Post und bitte teilen Sie uns dies telefonisch oder schriftlich mit.
+                </p>
+                <h3>Änderung</h3>
+                <p>Diese Allgemeine Geschäftsbedingung kann von geknüpft.ch jederzeit und ohne Angabe von Gründen
+                    geändert werden. Die Änderungen gelten nicht für bereits getätigte Bestellungen.
+                </p>
+                <h3>Gerichtsstand</h3>
+                <p>Gerichtsstand ist 8304 Wallisellen. Es gilt Schweizer Recht.
+                </p>
+                <h3>Schlussbestimmung</h3>
+                <p>Sollte eine Bestimmung dieser AGB unwirksam sein, bleiben die übrigen Bestimmungen
+                    unberührt. Für Streitigkeiten die über diese AGB hinausgehen, findet das Schweizerische
+                    Obligationenrecht Anwendung.
+                </p>
             </div>
         </div>
-        <
-        / Page >
-        );
+    </Page>
+);
 
-        export default Agb;
+export default Agb;
