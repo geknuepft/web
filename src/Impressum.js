cbb3eff1e2fd08a9a0fd0d565eeb3847ced9ca05
Fiona Koestler <fiona@koestler.ch>
2017-08-31 12:04:54 +0200

    add text for impressum

--- src/Impressum.js
+++ src/Impressum.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Page from './Page';
+import {Link} from 'react-router-dom';
 import config from 'react-global-configuration';
 
 const Impressum = () => (
@@ -7,12 +8,25 @@ const Impressum = () => (
         title="Impressum"
         headImagePath={config.get('apiUrl') + 'Image/800p/content/head/artikel_15.jpg'}
     >
-        <div>
-            <div className="intro">
-                <div className="intro-desc wide">
-                    <p>Text für Impressum.
-                    </p>
-                </div>
+        <div className="intro">
+            <div className="intro-desc wide">
+                <h4>Die Webseite www.geknüpft.ch wird betrieben von:</h4>
+                <p>Familie Koestler<br/>
+                    Richtiring 22<br/>
+                    8304 Wallisellen<br/>
+                    Schweiz
+                </p>
+                <h5>Kontakt:</h5>
+                <p>
+                    <a href="mailto:info@geknüpft.ch?Subject=info@geknüpft.ch"
+                       target="_top">info@geknüpft.ch</a><br/>
+                    oder über das <Link to="/Kontakt">Kontaktformular</Link>
+                </p>
+                <h5>Nutzung von Inhalt und Bildmaterial:</h5>
+                <p>Sofern nicht anders vermerkt, sind alle Inhalte dieser Webseite Eigentum der Familie Koestler. Die
+                    Nutzung unterliegt dem Urheberrecht. Kopien von Inhalten und Bildern dieser Webseite sind
+                    ausschliesslich für private Zwecke erlaubt. Alle anderen Nutzungen bedürfen unserer schriftlichen
+                    Zustimmung. Bitte nehmen Sie mit uns Kontakt auf.</p>
             </div>
         </div>
     </Page>
