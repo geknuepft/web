034221ed1dbc6bf407317d6736e9d2aed8d9f2b9
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2017-09-01 13:47:35 +0200

    make gallery fetch the instance list from the api

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,55 +1,137 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Gallery.css';
+import axios from 'axios';
 import config from 'react-global-configuration';
 
-const Gallery = () => (
-    <div>
-        <div className="intro">
-            <div className="intro-desc">
-                <p>Ihre Auswahl umfasst folgende <span className="bold">304</span> Aritkel:</p>
+const GallerySorter = () => (
+    <select className="inline">
+        <option value="new">Neuste zuoberst</option>
+        <option value="old">Älteste zuoberst</option>
+        <option value="price_up">Preis aufsteigend</option>
+        <option value="price_down">Preis absteigend</option>
+    </select>
+);
+
+class GalleryIntro extends Component {
+
+    static propTypes = {
+        numberOfItems: PropTypes.number.isRequired,
+    };
+
+    render() {
+        return (
+            <div className="intro">
+                <div className="intro-desc">
+                    <p>Ihre Auswahl umfasst folgende <span className="bold">{this.props.numberOfItems}</span> Aritkel:
+                    </p>
+                </div>
+                <div className="intro-desc right">
+                    <p>sortieren nach:
+                        <GallerySorter/>
+                    </p>
+                </div>
             </div>
-            <div className="intro-desc right">
-                <p>sortieren nach:
-                    <select className="inline">
-                        <option value="new">Neuste zuoberst</option>
-                        <option value="old">Älteste zuoberst</option>
-                        <option value="price_up">Preis aufsteigend</option>
-                        <option value="price_down">Preis absteigend</option>
-                    </select>
-                </p>
-            </div>
-        </div>
-        <ul className="article">
+        );
+    }
+}
+
+class GalleryItem extends Component {
+
+    static propTypes = {
+        instanceId: PropTypes.number.isRequired,
+        articleId: PropTypes.number.isRequired,
+        lengthMm: PropTypes.number.isRequired,
+        widthMm: PropTypes.number.isRequired,
+        picture0: PropTypes.string.isRequired,
+        priceCchf: PropTypes.number.isRequired,
+        discountCchf: PropTypes.number.isRequired,
+    };
+
+    render() {
+        const props = this.props;
+
+        return (
             <li>
                 <div className="article-img">
-                    <img src={config.get('imageUrl') + '150p/cma0/160523_1/IMG_3079.JPG'}
-                         alt="Makroaufnahme"/>
+                    <img src={config.get('imageUrl') + '150p/' + props.picture0} alt="Makroaufnahme"/>
                 </div>
-                <div className="article-title">Artikel-Nr.</div>
+                <div className="article-title">Artikel-Nr. {props.articleId}</div>
                 <table>
                     <tbody>
                     <tr>
                         <td>Länge</td>
-                        <td className="article-desc">Länge in cm</td>
+                        <td className="article-desc">{props.lengthMm / 10} cm</td>
                     </tr>
                     <tr>
                         <td>Breite</td>
-                        <td className="article-desc">&#126;Breite in cm</td>
+                        <td className="article-desc">&#126;{props.widthMm} mm</td>
                     </tr>
                     </tbody>
                 </table>
-                <div className="article-prize">CHF 16.00</div>
+                <div className="article-prize">CHF {(props.priceCchf - props.discountCchf) / 100}
+                    <span>({props.discountCchf / 100} Rabatt)</span>
+                </div>
             </li>
-            <li className="empty"/>
-            <li className="empty"/>
-            <li className="empty"/>
-            <li className="empty"/>
-            <li className="empty"/>
-            <li className="empty"/>
-            <li className="empty"/>
-            <li className="empty"/>
-        </ul>
-    </div>
-);
+        );
+    }
+}
+
+class Gallery extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            instanceList: null,
+        }
+    }
+
+    fetchDataFromApi = () => {
+        axios.get(config.get('apiUrl') + 'Instance')
+            .then(res => {
+                this.setState({instanceList: res.data});
+            });
+    };
+
+    componentDidMount() {
+        this.fetchDataFromApi();
+    }
+
+    render() {
+        // render nothing if list is not loaded
+        if (this.state.instanceList === null) {
+            return null;
+        }
+
+        return (
+            <div>
+                <GalleryIntro numberOfItems={this.state.instanceList.length}/>
+                <ul className="article">
+                    {this.state.instanceList.map((instance) =>
+                        <GalleryItem
+                            key={instance.instanceId}
+                            instanceId={instance.instanceId}
+                            articleId={instance.articleId}
+                            lengthMm={instance.lengthMm}
+                            widthMm={instance.widthMm}
+                            picture0={instance.picture0}
+                            priceCchf={instance.priceCchf}
+                            discountCchf={instance.discountCchf}
+                        />
+                    )}
+                    <li className="empty"/>
+                    <li className="empty"/>
+                    <li className="empty"/>
+                    <li className="empty"/>
+                    <li className="empty"/>
+                    <li className="empty"/>
+                    <li className="empty"/>
+                    <li className="empty"/>
+                </ul>
+            </div>
+        );
+    }
+}
 
 export default Gallery;
