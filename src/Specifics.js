22a639539c36e58ed816f1237b732dd325cd97d2
Fiona Koestler <fiona@koestler.ch>
2017-09-02 12:09:02 +0200

    specifics: change details, add ids and desc

--- src/Specifics.js
+++ src/Specifics.js
@@ -6,16 +6,16 @@ import config from 'react-global-configuration';
 const ArticleExist = () => (
     <div className="article-exist">
         <span className="detail-desc-title">Armband mit Eingeknüpftem</span>
+        <div className="detail-desc-ids">
+            <div>A 1100</div>
+            <div>I 1234</div>
+            <div>P 509</div>
+        </div>
+        <div className="detail-desc-de">
+            <p>Hier steht eine Beschreibung zum dem Artikel, sofern etwas in der Datenbank vorhanden ist im Feld article_desc.</p>
+        </div>
         <table className="tech-data">
             <tbody>
-            <tr>
-                <td>Artikel-Nr.</td>
-                <td className="article-desc">1100</td>
-            </tr>
-            <tr>
-                <td>Instanz-Nr.</td>
-                <td className="article-desc">1234</td>
-            </tr>
             <tr>
                 <td>Länge</td>
                 <td className="article-desc">&#126; 14 cm</td>
@@ -31,6 +31,15 @@ const ArticleExist = () => (
             <tr>
                 <td>Anzahl Fäden</td>
                 <td className="article-desc">18</td>
+            </tr>
+            <tr>
+                <td>Musterwiederholung
+                    <div className="tooltip">
+                        <button className="info">i</button>
+                        <span className="tooltiptext">Info</span>
+                    </div>
+                </td>
+                <td className="article-desc">alle 4 cm</td>
             </tr>
             <tr>
                 <td>Garntyp</td>
@@ -49,19 +58,6 @@ const ArticleExist = () => (
                 <td className="article-desc">20x Japanische Roccailles<br/>1x Silberfaden</td>
             </tr>
             <tr>
-                <td>Muster</td>
-                <td className="article-desc">Nr. 209</td>
-            </tr>
-            <tr>
-                <td>Musterwiederholung
-                    <div className="tooltip">
-                        <button className="info">i</button>
-                        <span className="tooltiptext">Info</span>
-                    </div>
-                </td>
-                <td className="article-desc">4 cm</td>
-            </tr>
-            <tr>
                 <td>Abschluss</td>
                 <td className="article-desc">Zopf</td>
             </tr>
@@ -70,52 +66,7 @@ const ArticleExist = () => (
                 <td className="article-desc">2-5 Tage</td>
             </tr>
             <tr>
-                <td className="spacer" colSpan={2}/>
-            </tr>
-            <tr>
-                <td>Preis</td>
-                <td className="article-desc">CHF 18.00</td>
-            </tr>
-            <tr>
-                <td>Rabatt</td>
-                <td className="article-desc">20%</td>
-            </tr>
-            <tr>
-                <td>Total</td>
-                <td className="article-desc"><span className="bold">CHF 16.00</span></td>
-            </tr>
-            </tbody>
-        </table>
-        <button type="submit">in den Warenkorb</button>
-    </div>
-);
-
-const ArticleComputed = () => (
-    <div className="article-computed">
-        <table className="tech-data">
-            <tbody>
-            <tr>
-                <td>Gewünschte Länge</td>
-                <td className="article-desc">
-                    <input type="number" step="0.5" name="length" defaultValue={14.5} min="10" max="30"
-                           className="inline"/>cm
-                </td>
-            </tr>
-            <tr>
-                <td>Abschluss</td>
-                <td className="article-desc">
-                    <select required className="inline">
-                        <option value="zopf">Zopf</option>
-                        <option value="lose">lose Fäden</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Lieferzeit</td>
-                <td className="article-desc">14 Tage</td>
-            </tr>
-            <tr>
-                <td className="spacer" colSpan={2}/>
+                <td className="spacer-line" colSpan={2}/>
             </tr>
             <tr>
                 <td>Preis</td>
@@ -148,7 +99,6 @@ const Specifics = () => (
             </div>
             <div className="detail-desc">
                 <ArticleExist/>
-                <ArticleComputed/>
             </div>
         </div>
     </div>
