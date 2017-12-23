6348e9ddf439c0b105838ff60152df25d9e2ff68
Lorenz Koestler <lorenz@koestler.ch>
2017-12-23 17:10:49 +0100

    add fullscreen modal

--- src/ImageScroller.js
+++ src/ImageScroller.js
@@ -2,10 +2,9 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './ImageScroller.css';
 import SmartImage from './SmartImage';
-import {Carousel} from 'react-bootstrap'
+import {Carousel, Modal} from 'react-bootstrap'
 
 class ImageScroller extends Component {
-
     static propTypes = {
         images: PropTypes.array.isRequired,
     };
@@ -14,7 +13,7 @@ class ImageScroller extends Component {
         super(props);
 
         this.state = {
-            fullscreen: false,
+            fullScreen: false,
         }
     }
 
@@ -22,16 +21,44 @@ class ImageScroller extends Component {
         // empty gallery
         if (this.props.images.length < 1) return null;
 
-        return <Carousel>
-            {this.props.images.map(
-                (image, idx) => <Carousel.Item key={idx}>
-                    <SmartImage path={image.path} height={600}/>
-                    <Carousel.Caption>
-                        <h3>{image.imageType}</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            )}
-        </Carousel>
+        let FullScreenModal = null;
+        if (this.state.fullScreen) {
+            FullScreenModal =
+                <Modal show={true}
+                       onHide={() => this.setState({fullScreen: false})}
+                       dialogClassName="image-scroller-modal">
+                    <Modal.Header closeButton>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Carousel>
+                            {this.props.images.map(
+                                (image, idx) =>
+                                    <Carousel.Item key={idx}>
+                                        <SmartImage path={image.path} height={900}/>
+                                        <Carousel.Caption>
+                                            <h3>{image.imageType}</h3>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                            )}
+                        </Carousel>
+                    </Modal.Body>
+                </Modal>;
+        }
+
+        return <div className="image-scroller">
+            <Carousel>
+                {this.props.images.map(
+                    (image, idx) =>
+                        <Carousel.Item key={idx} onClick={() => (this.setState({fullScreen: true}))}>
+                            <SmartImage path={image.path} height={600}/>
+                            <Carousel.Caption>
+                                <h3>{image.imageType}</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                )}
+            </Carousel>
+            {FullScreenModal}
+        </div>
     }
 }
 
