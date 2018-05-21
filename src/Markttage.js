77a2c17e415699bb0a265ed8c2ee1a335f7f2c87
Fiona Koestler <fiona@koestler.ch>
2018-05-21 17:37:19 +0200

    fix page and table of Markttage

--- src/Markttage.js
+++ src/Markttage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Page from './Page';
 import config from 'react-global-configuration';
+import ContentLayouter from "./ContentLayouter"
+import Textbox from './Textbox'
 import SmartImage from './SmartImage';
 
 const Markttage = () => (
@@ -8,109 +10,114 @@ const Markttage = () => (
         title="Markttage"
         headImagePath={config.get('imageUrl') + '800p/content/head/artikel_7.jpg'}
     >
-        <div className="intro">
-            <div className="intro-desc">
-                <p>Unsere handgefertigkten Artikel präsentieren wir regelmässig an verschiedenen Märkten
-                    in
-                    der Region Winterthur und Zürich, u.a. am Kunst- und Handwerkermarkt in Winterthur, am
-                    Walliseller
-                    Frühlingsmarkt und am Weltmarkt
-                    in Zürich Oerlikon. Die genauen Daten, Zeiten und Lokalitäten sind untenstehend zu finden
-                    und
-                    werden laufend ergänzt. Wir freuen uns auf Ihren persönlichen Besuch!</p>
-            </div>
-            <div className="intro-desc">
-                <p>Das Wetter macht leider nicht immer mit. Spätestens am Vorabend jedes Marktes entscheiden wir, ob es
-                    genügend trocken ist, um am Markt teilzunehmen und schalten dies hier auf.</p>
-            </div>
-        </div>
-        <div className="intro">
-            <div className="intro-desc">
-                <h2>Kunst und Handwerkermarkt Winterthur</h2>
-                <table>
-                    <tr>
-                        <th>Datum:</th>
-                        <td>15. September 2017</td>
-                    </tr>
-                    <tr>
-                        <th>Zeit:</th>
-                        <td>8 Uhr bis 16 Uhr</td>
-                    </tr>
-                    <tr>
-                        <th>Ort:</th>
-                        <td><a href="http://map.search.ch/d/zdkodrmzd" target="_blank">Winterthur, im Neumarkt (s.
-                            Karte)</a></td>
-                    </tr>
-                </table>
-                <p>Am Kunst- und Handwerkermarkt präsentieren zwischen 10 und 20 Ständen aus der Region ihre
-                    selbst hergestellen Produkte.</p>
-            </div>
-            <div className="intro-desc">
+        <div>
+            <ContentLayouter>
+                <Textbox>
+                    <p>Unsere handgefertigkten Artikel präsentieren wir regelmässig an verschiedenen Märkten
+                        in
+                        der Region Winterthur und Zürich, u.a. am Kunst- und Handwerkermarkt in Winterthur, am
+                        Walliseller
+                        Frühlingsmarkt und am Weltmarkt
+                        in Zürich Oerlikon. Die genauen Daten, Zeiten und Lokalitäten sind untenstehend zu finden
+                        und
+                        werden laufend ergänzt. Wir freuen uns auf Ihren persönlichen Besuch!</p>
+                </Textbox>
+                <Textbox>
+                    <p>Das Wetter macht leider nicht immer mit. Spätestens am Vorabend jedes Marktes entscheiden wir, ob
+                        es
+                        genügend trocken ist, um am Markt teilzunehmen und schalten dies hier auf.</p>
+                </Textbox>
+            </ContentLayouter>
+            <ContentLayouter>
+                <Textbox>
+                    <h2>Kunst- und Handwerkermarkt Winterthur</h2>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <th>Datum:</th>
+                            <td>15. September 2017</td>
+                        </tr>
+                        <tr>
+                            <th>Zeit:</th>
+                            <td>8 Uhr bis 16 Uhr</td>
+                        </tr>
+                        <tr>
+                            <th>Ort:</th>
+                            <td><a href="http://map.search.ch/d/zdkodrmzd" target="_blank">Winterthur, im Neumarkt (s.
+                                Karte)</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <p>Am Kunst- und Handwerkermarkt präsentieren zwischen 10 und 20 Ständen aus der Region ihre
+                        selbst hergestellen Produkte.</p>
+                </Textbox>
                 <SmartImage
                     path="/content/box/khw.jpg"
                     height={800}
                     fullscreen={true}
                     alt="Marktstand Winterthur"
                 />
-            </div>
-        </div>
-        <div className="intro">
-            <div className="intro-desc">
-                <h2>Riedenermarkt Wallisellen</h2>
-                <table>
-                    <tr>
-                        <th>Datum:</th>
-                        <th>30. September 2017</th>
-                    </tr>
-                    <tr>
-                        <th>Zeit:</th>
-                        <td>10 Uhr bis 17 Uhr</td>
-                    </tr>
-                    <tr>
-                        <th>Ort:</th>
-                        <td><a href="http://map.search.ch/d/zdkodrmzd" target="_blank">Wallisellen, in der
-                            Bahnhofstrasse</a></td>
-                    </tr>
-                </table>
-                <p>Jedes Jahr organisiert der Gewerbeverein Wallisellen ein buntes Markttreiben in der
-                    Bahnhofstrasse.</p>
-            </div>
-            <div className="intro-desc">
+            </ContentLayouter>
+            <ContentLayouter>
+                <Textbox>
+                    <h2>Riedenermarkt Wallisellen</h2>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <th>Datum:</th>
+                            <td>15. September 2017</td>
+                        </tr>
+                        <tr>
+                            <th>Zeit:</th>
+                            <td>8 Uhr bis 16 Uhr</td>
+                        </tr>
+                        <tr>
+                            <th>Ort:</th>
+                            <td><a href="http://map.search.ch/d/zdkodrmzd" target="_blank">Wallisellen, in der
+                                Bahnhofstrasse</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <p>Jedes Jahr organisiert der Gewerbeverein Wallisellen ein buntes Markttreiben in der
+                        Bahnhofstrasse.</p>
+                </Textbox>
                 <SmartImage
                     path="/content/box/wallisellen.jpg"
                     height={800}
                     fullscreen={true}
                     alt="Marktstand Wallisellen"
                 />
-            </div>
-        </div>
-        <div className="intro">
-            <div className="intro-desc">
-                <h2>Kunst- und Handwerkermarkt Winterthur</h2>
-                <table>
-                    <tr>
-                        <th>Datum:</th>
-                        <th>30. Oktober 2017</th>
-                    </tr>
-                    <tr>
-                        <th>Zeit:</th>
-                        <td>9 Uhr bis 16 Uhr</td>
-                    </tr>
-                    <tr>
-                        <th>Ort:</th>
-                        <td><a href="http://map.search.ch/d/zdkodrmzd" target="_blank">Winterthur, im Neumarkt (s.
-                            Karte)</a></td>
-                    </tr>
-                </table>
-            </div>
-            <div className="intro-desc">
+            </ContentLayouter>
+            <ContentLayouter>
+                <Textbox>
+                    <h2>Kunst- und Handwerkermarkt Winterthur</h2>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <th>Datum:</th>
+                            <td>30. Oktober 2017</td>
+                        </tr>
+                        <tr>
+                            <th>Zeit:</th>
+                            <td>9 Uhr bis 16 Uhr</td>
+                        </tr>
+                        <tr>
+                            <th>Ort:</th>
+                            <td><a href="http://map.search.ch/d/zdkodrmzd" target="_blank">Winterthur, im Neumarkt (s.
+                                Karte)</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <p>Jedes Jahr organisiert der Gewerbeverein Wallisellen ein buntes Markttreiben in der
+                        Bahnhofstrasse.</p>
+                </Textbox>
                 <SmartImage
                     path="/content/box/khw_winter.jpg"
                     height={800}
                     fullscreen={true}
                     alt="Marktstand Winterthur im Winter"
                 />
-            </div>
+            </ContentLayouter>
         </div>
     </Page>
 );
